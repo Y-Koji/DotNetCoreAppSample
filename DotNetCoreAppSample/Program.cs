4fd80044f12259f990b5d3335a5a8fb349f69dcf
安富 孝司 <koji_micro@hotmail.co.jp>
2018-10-27 21:01:39 +0900

    プロジェクト ファイルを追加します。

---
+++ DotNetCoreAppSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreAppSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
